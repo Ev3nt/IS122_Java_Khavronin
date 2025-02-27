7a6fae74181173364ce4b9e866ed62f694e1b85e
Богдан Прогер <ev3nt.official@gmail.com>
2025-02-27 23:51:55 +0300

    UTF-8 installed as a main charset.

--- src/main/java/org/ev3nt/Main.java
+++ src/main/java/org/ev3nt/Main.java
@@ -4,8 +4,23 @@ import org.ev3nt.gui.classes.Menu;
 import org.ev3nt.modes.classes.AudienceSchedule;
 import org.ev3nt.modes.classes.GroupSchedule;
 
+import javax.swing.*;
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
+
+        try {
+            Field charsetField = Charset.class.getDeclaredField("defaultCharset");
+            charsetField.setAccessible(true);
+            charsetField.set(null, null);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            JOptionPane.showMessageDialog(null, "Не удалось установить кодировку UTF-8, пожалуйста, установите её вручную!", "Сообщение", JOptionPane.WARNING_MESSAGE);
+        }
+
+
         Menu menu = new Menu("Test Window", 300, 200);
         menu.run();
 
--- src/main/java/org/ev3nt/web/classes/HttpSchedule.java
+++ src/main/java/org/ev3nt/web/classes/HttpSchedule.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class HttpSchedule implements WebSchedule {
@@ -14,7 +15,9 @@ public class HttpSchedule implements WebSchedule {
         String response = "";
 
         try {
-            URL url = new URL("https://scala.mivlgu.ru/core/frontend/index.php?r=schedulecash/group&group=" + URLEncoder.encode(group) + "&semester=" + semester + "&year=" + year + "&format=json");
+            String encodedGroup = URLEncoder.encode(group, StandardCharsets.UTF_8.toString());
+
+            URL url = new URL( "https://scala.mivlgu.ru/core/frontend/index.php?r=schedulecash/group&group=" + encodedGroup + "&semester=" + semester + "&year=" + year + "&format=json");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
